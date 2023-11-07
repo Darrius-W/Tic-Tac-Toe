2b68ac10cae64aba22b3019e8fd2c8d5a5851634
Darrius-W <darriuswhite435@gmail.com>
2023-11-07 11:09:15 -0600

    Initializing of variables

---
+++ Tic-Tac-Toe/main.js
@@ -0,0 +1,11 @@
+let count = 0;
+const win = [[1,2,3], [4,5,6], [7,8,9],
+             [1,4,7], [2,5,8], [3,6,9],
+             [1,5,9], [3,5,7]
+            ];
+let arrX = [];// Array to hold all ids that have had their symbols switched
+let arrO = [];
+let countX = 0;
+let countO = 0;
+let playXwins = 0;
+let playOwins = 0;
